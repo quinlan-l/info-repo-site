7158c7e6db8f08c8563b62a356f3cf83eb3dff05
quinlan-l <quinlan_l@qq.com>
2022-10-10 11:31:41 -0700

    search navigation config

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -3,6 +3,6 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   { text: '家', icon: 'home', link: '/' },
   { text: '道', icon: 'creative', link: '/dao/' },
-  { text: '搜', icon: 'search', link: "/tools/search-nav.html" },
+  { text: '器', icon: 'search', link: "/tools/" },
   { text: 'vuepress-theme-hope', icon: 'link', link: 'https://vuepress-theme-hope.github.io/v2/zh/' },
 ]);
---
+++ docs/tools/README.md
@@ -0,0 +1,13 @@
+---
+category: 
+  - README
+tag:
+  - 概述
+
+article: false
+timeline: false
+---
+
+## 工具集
+
+* [聚合搜索引擎](https://docs.1369.ml/tools/search-nav.html)
