05784dc1dfee7161fbbfb327ce61b1c989792d44
quinlan-l <quinlan_l@qq.com>
2022-10-10 10:55:47 -0700

    put search navigation into docs-nav

--- docs/.vuepress/navbar/zh.ts
+++ docs/.vuepress/navbar/zh.ts
@@ -3,5 +3,6 @@ import { navbar } from "vuepress-theme-hope";
 export const zhNavbar = navbar([
   { text: '家', icon: 'home', link: '/' },
   { text: '道', icon: 'creative', link: '/dao/' },
+  { text: '搜', icon: 'search', link: "/tools/search-nav.html" },
   { text: 'vuepress-theme-hope', icon: 'link', link: 'https://vuepress-theme-hope.github.io/v2/zh/' },
 ]);
